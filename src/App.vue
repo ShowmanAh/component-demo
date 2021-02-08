53917d19ccce02f861f206b271668aa2c85fa70a
Showman <showman.sh.ahmed@gmail.com>
2021-02-08 13:04:09 +0200

    add new quote

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
  <div class="container">
+   <app-new-quote @quoteAdded="newQuote"></app-new-quote>
    <app-quote-grid :quotes="quotes"></app-quote-grid>
    </div> 
 </template>
 <script>
-import QuoteGride from './components/QuoteGride';
+import QuoteGride from './components/QuoteGride.vue';
+import NewQuote from './components/NewQuote.vue';
 export default {
  data: function(){
    return {
@@ -14,8 +16,14 @@ export default {
      maxQuote: 10
    }
  },
+ methods: {
+   newQuote: function(quote){
+     this.quotes.push(quote);
+   }
+ },
  components: {
-   appQuoteGrid: QuoteGride
+   appQuoteGrid: QuoteGride,
+   appNewQuote: NewQuote 
  } 
 }
 </script>
